2c25bab2055aed87272626799fec135a98ff294e
Unicorn Rainbows <me@hermancoetzee.com>
2019-01-28 17:14:11 +0200

    Testing config

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
+	'hostname' => 'localhost:8888',
+	'username' => 'root',
+	'password' => 'root',
 	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
